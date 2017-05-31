6b09438a8732d425ffe03b1205130d151d09dcb5
Blerine <blerine.tasholli@helsinki.fi>
2017-05-31 12:49:06 +0300

    update luokkakaavio, update checkstyle, added mysuppressions and update Kayttoliittyma

--- Pinta-alaJaTilavuusLaskin/src/main/java/fi/blerine/laskin/kayttoliittyma/Kayttoliittyma.java
+++ Pinta-alaJaTilavuusLaskin/src/main/java/fi/blerine/laskin/kayttoliittyma/Kayttoliittyma.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
 
 public class Kayttoliittyma implements Runnable {
@@ -29,17 +30,25 @@ public class Kayttoliittyma implements Runnable {
     }
 
     private void luoKomponentit(Container container) {
-        GridLayout layout = new GridLayout(2, 1);
+        GridLayout layout = new GridLayout(3, 2);
         container.setLayout(layout);
         
         JButton lierioNappi = new JButton("Lieriö");
         JButton kartioNappi = new JButton("Kartio");
+        JButton ympyraNappi = new JButton("Ympyrä");
+        JButton nelioNappi = new JButton("Neliö");
+        JTextArea sadeTaiKantaTeksti = new JTextArea("Säde/Kanta senttimetreinä: \n");
+        JTextArea korkeusTeksti = new JTextArea("Korkeus senttimetreinä: \n");
         
         container.add(lierioNappi);
         container.add(kartioNappi);
+        container.add(ympyraNappi);
+        container.add(nelioNappi);
+        container.add(sadeTaiKantaTeksti);
+        container.add(korkeusTeksti);
+        
 
-        JLabel teksti = new JLabel("Testiä...");
-        container.add(teksti);
+ 
     }
 
     public JFrame getFrame() {
